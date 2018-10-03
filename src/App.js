85a411e54f57454c564b6888fcb193b5174b76cd
KhoroshN <horosh3d@gmail.com>
2018-10-03 16:31:10 +0300

    rio min live ver

--- src/App.js
+++ src/App.js
@@ -24,17 +24,11 @@ class App extends Component {
   readColor = (color) => {
         switch (color) {
           case "yellow": this.send('blue');
-            break;
           case "grey": this.send('red');
-            break;
           case "red":this.send('green');
-            break;
           case "green":this.send('blue');
-            break;
           case "blue": this.send('grey');
-            break;
           default: this.send('white');
-          break;
         }
   }
 
@@ -43,20 +37,20 @@ class App extends Component {
     //const socket = socketIOClient(this.state.endpoint);
      this.socket.on('change color', (col) => {
        document.body.style.backgroundColor = col
-       //this.setColor(col)
      })
 
     return (
-      <div style={{ textAlign: "center" }}>
+    <div className="Rio">
+      <div style={{ textAlign: "center", position: "absolute" }}>
         <button onClick={() => this.send() }>Clear Color</button>
         <button id="blue" onClick={() => this.send('blue')}>Blue</button>
         <button id="red" onClick={() => this.send('red')}>Red</button>
         <button id={this.state.color} onClick={(e) => this.readColor(this.state.color)}>Current {this.state.color}, Click to Switch next </button>
-
-
-        {Three}
-
       </div>
+      <div>
+        {Three}
+      </div>
+    </div>
     )
   }
 }
--- src/Three.js
+++ src/Three.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 
 var camera, scene, renderer;
-var geometry, material, mesh;
+var geometry, material, mesh, grid;
  
 init();
 animate();
@@ -9,20 +9,47 @@ animate();
 function init() {
  
     camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+    camera.position.x = 1;
+    camera.position.y = 1;
     camera.position.z = 1;
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
  
     scene = new THREE.Scene();
  
+    var size = 1;
+    var divisions = 10;
+
+    var gridHelper = new THREE.GridHelper( size, divisions );
+    scene.add( gridHelper );
+
     geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
     material = new THREE.MeshNormalMaterial();
  
     mesh = new THREE.Mesh( geometry, material );
     scene.add( mesh );
+
+    geometry = new THREE.PlaneGeometry( 1, 0.5 );
+
+    grid = new THREE.Mesh( geometry, material );
+    grid.rotation.set(0, 0, Math.PI/2);
+    scene.add( grid );
  
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer = new THREE.WebGLRenderer( { alpha: true, antialias: true } );
     renderer.setSize( window.innerWidth, window.innerHeight );
+
+    //document.getElementById('cnv').appendChild( renderer.domElement );
+
     document.body.appendChild( renderer.domElement );
  
+    window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+
+function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize( window.innerWidth, window.innerHeight );
 }
  
 function animate() {
